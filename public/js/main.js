fab10716e22b7128733efe12575803011d630f5b
Gabay1954 <gabrielcocuspoutlook.fr>
2020-11-03 17:49:12 +0100

    mobile responsive modif

--- public/js/main.js
+++ public/js/main.js
@@ -102,7 +102,7 @@ function outputMessage(message) {
         document.querySelector('.chat-messages').appendChild(divusername);
 
         pdate.classList.add('text-date');
-        pdate.innerHTML += `${(" " + date.toLocaleString("fr-FR"))}`;
+        pdate.innerHTML += `<span>${(" " + date.toLocaleString("fr-FR"))}</span>`;
         divusername.appendChild(pdate);
       }
 
