0ab837a8289585081862b1ca3630190e001b4933
Gabay1954 <gabrielcocuspoutlook.fr>
2020-11-03 10:10:50 +0100

    commentaire chat

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ io.on('connection', socket => {
       .to(user.room)
       .emit(
         'message',
-        formatMessage(botName, `${user.username} has joined the chat`)
+        formatMessage(botName, `${user.username} à rejoint le chat`)
       );
 
     // Envoyer infos utilisateurs / rooms
@@ -58,7 +58,7 @@ io.on('connection', socket => {
     if (user) {
       io.to(user.room).emit(
         'message',
-        formatMessage(botName, `${user.username} has left the chat`)
+        formatMessage(botName, `${user.username} à quitté le chat`)
       );
 
       // envoyer info utilisateur et room
@@ -72,4 +72,4 @@ io.on('connection', socket => {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`serveur tourne sur le port ${PORT}`));
